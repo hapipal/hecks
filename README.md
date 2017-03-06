94dfe8b032cf7de68f04834b338e624ad6a10768
Devin Ivy <devin@bigroomstudios.com>
2017-03-06 00:33:44 -0500

    Initial commit :monkey:

---
+++ README.md
@@ -0,0 +1,10 @@
+# hecks
+
+Mount your express app onto your hapi server, aw heck!
+
+[![Build Status](https://travis-ci.org/devinivy/hecks.svg?branch=master)](https://travis-ci.org/devinivy/hecks) [![Coverage Status](https://coveralls.io/repos/devinivy/hecks/badge.svg?branch=master&service=github)](https://coveralls.io/github/devinivy/hecks?branch=master)
+
+## Usage
+```js
+// :o)
+```
---
+++ test/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Load modules
+
+const Lab = require('lab');
+const Code = require('code');
+const Hecks = require('..');
+
+// Test shortcuts
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+describe('Hecks', () => {
+
+    it('doesn\'t have tests yet.', (done) => {
+
+        done(new Error('TODO'));
+    });
+});
